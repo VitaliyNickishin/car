527d986c7bf04958646531bcbb57974ef2437117
“VitaliyNickishin” <nickishin34@gmail.com>
2018-08-10 01:57:58 +0300

    ready nav and details

--- app/js/main.js
+++ app/js/main.js
@@ -2,6 +2,34 @@ $(document).ready(function(){
 
 
 
+
+	$("#min").click(function() { 
+        //Отображаем скрытый блок 
+        $(".package-min").fadeIn(); 
+        
+         $(".package-min").fadeOut(); 	
+       
+      });
+	$("#standart").click(function() { 
+        //Отображаем скрытый блок 
+        $(".package-standart").fadeIn(); 
+         
+         $(".package-standart").fadeOut(); 
+       
+      });
+/*
+	$(function() {
+	  $('#min').click(function() {
+	    $('.package-min').toggle('slow');
+	  });
+	});
+	$(function() {
+	  $('#standart').click(function() {
+	    $('.package-standart').toggle('slow');
+	  });
+	});
+*/
+
 }); 
 
 
