7f708e91fe67c0a8b378a5c88c67762fb5776e0d
“VitaliyNickishin” <nickishin34@gmail.com>
2018-08-22 01:49:10 +0300

    work with jquerry for nav

--- app/js/main.js
+++ app/js/main.js
@@ -1,19 +1,63 @@
 $(document).ready(function(){
+/*
+var flag=true;
+    $("#min").click(function()
+        {
+            if(flag==true)
+            {
+                $(".package-min").fadeIn();
+                flag=!flag;
+            }
+            else
+            {
+                $(".package-min").fadeOut();
+                flag=!flag;
+            }
+    });
+*/
+$('#min').click(
+  function(){
+    $('.package-min').fadeIn('slow',0.5);
+  },
+  function(){
+    $('.package-min').fadeOut('normal',1);
+  }
+);
 
 /*
         $('#min').click(function(){
 
-            $('.package-min').fadeIn();
+            $('.package-min').css(
+              {'display', 'block'})
         });
-        $('#standart').click(function(){
-            $('.package-min').fadeOut();
-            $('.package-standart').fadeIn();
+        $('html').click(function(){
+
+            $('.package-min').css(
+              {'display', 'none'})
         });
- 
+ */
+ /*
+ $('#min').click(function (e){ // отслеживаем событие клика по веб-документу
+        var block = $(".package-min"); // определяем элемент, к которому будем применять условия (можем указывать ID, класс либо любой другой идентификатор элемента)
+        if (!block.is(e.target) // проверка условия если клик был не по нашему блоку
+            && block.has(e.target).length === 0) { // проверка условия если клик не по его дочерним элементам
+            block.fadeIn(); // если условия выполняются - скрываем наш элемент
+        }else {
+    $(block).fadeOut();
+                       }
+    });
 */
 
-
-
+/*    
+$('#min').on('click', function(e) {
+        e.preventDefault();
+        $('.package-min').each(function() {
+            $(this).css('display', 'none');
+        });
+        var block = $(this).attr('href');
+        $(block).css('display', 'block');
+    });
+*/
 
 /*
 $('#min').click(function(){
@@ -70,6 +114,7 @@ $("#standart").click(function () {
        
       });
 */
+/*
 	$(function() {
 	  $('#min').click(function() {
 	    $('.package-min').toggle('slow');
@@ -95,7 +140,7 @@ $("#standart").click(function () {
 	    $('.package-standart-3plus').toggle('slow');
 	  });
 	});
-
+*/
 
 }); 
 
